c1e2188c82519ff35ef50aca5c45b596c243aa65
Eric Draegerdt <eric.draegerdt@posteo.de>
2025-09-19 16:57:08 +0200

    added route for adding new blogpost and html form to add new post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import json
 
 app = Flask(__name__)
@@ -9,6 +9,13 @@ def hello_world():
     with open('data/storage.json', 'r') as f:
         blog_posts = json.load(f)
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        # We will fill this in the next step
+        pass
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
